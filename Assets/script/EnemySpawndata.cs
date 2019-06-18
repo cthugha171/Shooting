106a6911ea12b9a3475b6c41bf8b936a79e64c05
kubota_syou <kirby8250@gmail.com>
2019-06-18 13:41:39 +0900

    過去のデータに置き換えた！

--- Assets/script/EnemySpawndata.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "EnemySpawnData", menuName = "EnemySpawnData")]
-public class EnemySpawndata : ScriptableObject
-{
-    public List<Status> statuses = null;
-}
--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float speed = 0.1f;
 
-    // Update is called once per frame
+    public camerashake shake;
     void Update()
     {
-        
+        //横移動
+        float z = Input.GetAxisRaw("Horizontal");
+        //縦移動
+        float y = Input.GetAxisRaw("Vertical");
+        //代入
+        this.transform.position += new Vector3(0, y, z) * speed;
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            shake.Shake(0.25f, 0.1f);
+        }
     }
 }
