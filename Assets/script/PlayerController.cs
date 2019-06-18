66ea23309fe0695be69b415f2716a7d96558fb4b
kubota_syou <kirby8250@gmail.com>
2019-06-18 10:22:12 +0900

    画面揺らしを追加したよ

--- Assets/script/PlayerController.cs
+++ Assets/script/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed = 0.1f;
+
+    public camerashake shake;
     void Update()
     {
         //横移動
@@ -13,5 +15,10 @@ public class PlayerController : MonoBehaviour
         float y = Input.GetAxisRaw("Vertical");
         //代入
         this.transform.position += new Vector3(0, y, z) * speed;
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            shake.Shake(0.25f, 0.1f);
+        }
     }
 }
