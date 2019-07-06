055ffcc01863cbb1a405203b5faed6240d1c2e93
Cthugha <tatsusato0519@gmail.com>
2019-07-06 16:11:53 +0900

    ステージのスクロール完成、enemyを少し修正

---
+++ Assets/script/BackGround.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround: MonoBehaviour
+{
+    [SerializeField] private float scrollSpead;
+    [SerializeField] private float tileSize;
+
+    private Vector3 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;//最初の位置を保存
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpead, tileSize);
+        transform.position = startPosition + Vector3.forward * newPosition;
+    }
+}
--- Assets/script/EnemySpawn.cs
+++ Assets/script/EnemySpawn.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class EnemySpawn : MonoBehaviour
 {
     [SerializeField] private GameObject[] enemy;
-    public List<GameObject> enemyList;
     private int number;
     private float posY;
     private int count;
@@ -22,18 +21,19 @@ public class EnemySpawn : MonoBehaviour
         if(count%630==0)
         {
             number = Random.Range(0,enemy.Length);
+            
             posY = Random.Range(-5, 5);
             if (number == 1 || number == 6)
             {
                 var _Enemy = Instantiate(enemy[number], transform.position, transform.rotation);
-                enemyList.Add(_Enemy);
-                Debug.Log("敵見つけたからリスト入れとく");
+
+                Destroy(_Enemy, 50.0f);
             }
             else
             {
                 var _Enemy = Instantiate(enemy[number], transform.position + new Vector3(0, posY, 0), transform.rotation);
-                enemyList.Add(_Enemy);
-                Debug.Log("敵見つけたからリスト入れとく");
+
+                Destroy(_Enemy, 50.0f);
             }
         }
 
